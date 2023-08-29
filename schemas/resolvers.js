d7d70cb10ed57f4797f3069975adcef2d04bd710
amandardz <amandardz15@gmail.com>
2023-08-29 16:33:57 -0500

    creating and testing user query and mutation to get all users and create users

--- schemas/resolvers.js
+++ schemas/resolvers.js
@@ -1,7 +1,17 @@
 const { User, Launch } = require('../models');
 
 const resolvers = {
+  Query: {
+    users: async () => {
+      return await User.find({});
+    }
+  }, 
 
+  Mutation: {
+    addUser: async(parent, args) => {
+      return await User.create(args);
+    }
+  }
 };
 
 module.exports = resolvers;
--- schemas/typeDefs.js
+++ schemas/typeDefs.js
@@ -1,5 +1,17 @@
 const typeDefs = `
+  type User {
+    _id: ID
+    username: String
+    email: String
+  }
 
+  type Query {
+    users: [User]
+  }
+
+  type Mutation {
+    addUser(username: String!, email: String!, password: String!): User
+  }
 `;
 
 module.exports = typeDefs;
