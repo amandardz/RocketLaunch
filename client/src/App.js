826c3d65a2d048298260b35bc285deae8e998512
amandardz <amandardz15@gmail.com>
2023-09-03 15:19:31 -0500

    configuring client and server side connection

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,19 @@
+import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLInk } from '@apollo/client';
+
+const httpLink = createHttpLInk({
+  uri: '/graphql', 
+});
+
+const client = new ApolloClient({
+  link: httpLink, 
+  cache: new InMemoryCache(), 
+});
+
 function App() {
   return (
-    <div>
-      Homepage
-    </div>
+    <ApolloProvider client={client}>
+      <div>Hello World!</div>
+    </ApolloProvider>
   );
 }
 
