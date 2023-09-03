a5648c6f08fc21bade68d95b3a715b071a306a87
amandardz <amandardz15@gmail.com>
2023-09-02 19:50:41 -0500

    setting up typeDefs/mutations to test out middleware JWT

--- schemas/resolvers.js
+++ schemas/resolvers.js
@@ -1,17 +1,37 @@
-const { User, Launch } = require('../models');
+const { User } = require("../models");
+const { signToken } = require("../utils/auth");
+const { AuthenticationError } = require('apollo-server-express');
 
 const resolvers = {
   Query: {
     users: async () => {
       return await User.find({});
-    }
-  }, 
+    },
+  },
 
   Mutation: {
-    addUser: async(parent, args) => {
-      return await User.create(args);
-    }
-  }
+    addUser: async (parent, args) => {
+      const user = await User.create(args);
+      const token = signToken(user);
+      return {token, user};
+    },
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        throw new AuthenticationError("Incorrect credentials");
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw new AuthenticationError("Incorrect credentials");
+      }
+
+      const token = signToken(user);
+      return { token, user };
+    },
+  },
 };
 
 module.exports = resolvers;
--- schemas/typeDefs.js
+++ schemas/typeDefs.js
@@ -4,13 +4,20 @@ const typeDefs = `
     username: String
     email: String
   }
+  
+  type Auth {
+    token: ID!
+    user: User
+  }
 
   type Query {
     users: [User]
+
   }
 
   type Mutation {
-    addUser(username: String!, email: String!, password: String!): User
+    addUser(username: String!, email: String!, password: String!): Auth
+    login(email: String!, password: String!): Auth
   }
 `;
 
