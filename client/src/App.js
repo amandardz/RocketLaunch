6b0e85bed160c37f95ed61e90bbde302826ed9a3
amandardz <amandardz15@gmail.com>
2023-09-06 22:43:04 -0500

    adding and displaying the components to the homepage

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,23 @@
-import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLInk } from '@apollo/client';
+// import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLInk } from '@apollo/client';
 
-const httpLink = createHttpLInk({
-  uri: '/graphql', 
-});
+// const httpLink = createHttpLInk({
+//   uri: '/graphql', 
+// });
 
-const client = new ApolloClient({
-  link: httpLink, 
-  cache: new InMemoryCache(), 
-});
+// const client = new ApolloClient({
+//   link: httpLink, 
+//   cache: new InMemoryCache(), 
+// });
+
+import Home from './pages/Home';
 
 function App() {
-  return (
-    <ApolloProvider client={client}>
-      <div>Hello World!</div>
-    </ApolloProvider>
-  );
-}
+  // return (
+  //   <ApolloProvider client={client}>
+  //     <div>Hello World!</div>
+  //   </ApolloProvider>
+  // );
+  return <Home />
+};
 
 export default App;
---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import MainContainer from "../components/MainContainer/MainContainer";
+import Header from "../components/Header/Header";
+
+const Home = () => {
+  return (
+    <MainContainer>
+      <Header />
+    </MainContainer>
+  );
+};
+
+export default Home;
