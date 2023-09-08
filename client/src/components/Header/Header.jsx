315998e5f80909b55cefa44bc83cf4caa284b8de
amandardz <amandardz15@gmail.com>
2023-09-08 00:15:49 -0500

    updating code for consistency

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,15 +1,15 @@
-import "./Header.css";
-import { Link } from "react-router-dom";
+import './Header.css';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
-    <div className="headerCard">
-      <h1 className="title">
+    <div className='headerCard'>
+      <h1 className='title'>
         <span>Rocket</span> Launch
       </h1>
-      <p className="description">where you can search for rocket launches</p>
+      <p className='description'>where you can search for rocket launches</p>
       <Link to='/credentials'>
-        <button className="headerBtn"> Login/Signup</button>
+        <button className='headerBtn'> Login/Signup</button>
       </Link>
     </div>
   );
