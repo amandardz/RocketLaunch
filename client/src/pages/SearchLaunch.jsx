d3bfb96d386386ab7019c876f328979a15e92656
amandardz <amandardz15@gmail.com>
2023-09-11 13:16:11 -0500

    setting up and testing the getLocationData function to get an array of launch locations

--- client/src/pages/SearchLaunch.jsx
+++ client/src/pages/SearchLaunch.jsx
@@ -1,7 +1,29 @@
 import SearchForm from '../components/SearchForm/SearchForm';
 
 const SearchLaunch = () => {
-  return <SearchForm />;
+  const getLocationData = async (country, state, startDate, endDate) => {
+    const locationIdsArr = [];
+
+    const locationData = await fetch(`https://ll.thespacedevs.com/2.2.0/location/?country_code=${country}&name__contains=${state}`);
+
+    if (!locationData.ok) {
+      throw new Error('something went wrong!');
+    }
+
+    const { results } = await locationData.json();
+
+    if(results.length === 0) {
+      return 'No data available with that search criteria';
+    } else {
+      for (let i = 0; i < results.length; i++) {
+        locationIdsArr.push(results[i].id);
+      }
+    }
+
+    console.log(locationIdsArr);
+  };
+
+  return <SearchForm getLocationData={getLocationData} />;
 };
 
 export default SearchLaunch;
