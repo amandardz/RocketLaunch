02c9127d920e6310df1fee95724c094379267e2c
amandardz <amandardz15@gmail.com>
2023-09-11 13:48:06 -0500

    setting up and testing getLaunchData function to retrieve the launch data needed

--- client/src/pages/SearchLaunch.jsx
+++ client/src/pages/SearchLaunch.jsx
@@ -8,7 +8,7 @@ const SearchLaunch = () => {
 
     if (!locationData.ok) {
       throw new Error('something went wrong!');
-    }
+    };
 
     const { results } = await locationData.json();
 
@@ -17,10 +17,23 @@ const SearchLaunch = () => {
     } else {
       for (let i = 0; i < results.length; i++) {
         locationIdsArr.push(results[i].id);
-      }
-    }
+      };
+    };
+    
+    getLaunchData(locationIdsArr, startDate, endDate);
+  };
 
-    console.log(locationIdsArr);
+  const getLaunchData = async(locationIdsArr, startDate, endDate) => {
+    const locationIds = locationIdsArr.join();
+    const launchData = await fetch(`https://ll.thespacedevs.com/2.2.0/launch/?launcher_config__id=${locationIds}&window_end__lte=${endDate}&window_start__gte=${startDate}`);
+    
+    if(!launchData.ok) {
+      throw new Error('something went wrong!');
+    };
+
+    const { results } = await launchData.json();
+
+    console.log(results);
   };
 
   return <SearchForm getLocationData={getLocationData} />;
