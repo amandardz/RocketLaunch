848acc2e89845c1fb0525765c832a180f4658850
amandardz <amandardz15@gmail.com>
2023-09-19 17:18:18 -0500

    testing to retrieve data from logged user and beginning to set up dashboard with that user's data

--- client/src/components/Dashboard/Dashboard.jsx
+++ client/src/components/Dashboard/Dashboard.jsx
@@ -1,7 +1,22 @@
 import Card from '../Card/Card';
+import NavBar from '../NavBar/NavBar';
+
+import { useQuery, useMutation } from '@apollo/client';
+import { QUERY_USER } from '../../utils/queries';
+import Button from '../Button/Button';
 
 const Dashboard = () => {
-  return <Card>This is the dashboard</Card>;
+  const { loading, data } = useQuery(QUERY_USER);
+  const userData = data?.user || {};
+  console.log(userData);
+  return (
+    <>
+      <NavBar />
+      <Card>
+        <h1>Welcome {userData.username}</h1>
+      </Card>;
+    </>
+  );
 };
 
 export default Dashboard;
