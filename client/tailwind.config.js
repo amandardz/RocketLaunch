ca722fa7fbf5d28c26966e3f368c3da31fffd383
amandardz <amandardz15@gmail.com>
2023-09-25 16:39:36 -0500

    implementing tailwind and font awesome

---
+++ client/tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+    colors: {
+      'space-blue': '#09243d',
+      'white': '#ffffff',
+      'light-blue': '#00cbf8',
+      'orange': '#f47f58'
+    }
+  },
+  plugins: [],
+}
+
